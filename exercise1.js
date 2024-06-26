81a4bf742dcefcea6b4f39d91f5e8ab4942ac6b2
videodave <13732631+videodave@users.noreply.github.com>
2024-06-26 11:41:53 -0500

    created project

---
+++ exercise1.js
@@ -0,0 +1,83 @@
+
+let humanScore = 0;
+let computerScore = 0;
+
+let result = Math.round(Math.random() * 3)
+console.log("this is the result - ", result)
+
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        return console.log("You tied")
+    }else if(humanChoice === "rock"){
+        if(computerChoice === "paper"){
+            computerScore++
+            console.log(`You lose - ${computerChoice} beats ${humanChoice}`)
+        }else{
+            humanScore++
+            console.log(`You win! - ${humanChoice} beats ${computerChoice}`)
+        }
+    }else if(humanChoice === "paper"){
+        if(computerChoice === "scissors"){
+            computerScore++
+            console.log(`You lose - ${computerChoice} beats ${humanChoice}`)
+        }else{
+            humanScore++
+            console.log(`You win! - ${humanChoice} beats ${computerChoice}`)
+        }
+    }else if(humanChoice === "scissors"){
+        if(computerChoice === "rock"){
+            computerScore++
+            console.log(`You lose - ${computerChoice} beats ${humanChoice}`)
+        }else{
+            humanScore++
+            console.log(`You win! - ${humanChoice} beats ${computerChoice}`)
+        }
+    }else{
+        return console.log("error")
+    }
+}
+
+
+function playGame(){
+    for (let index = 1; index <= 5; index++) {
+        console.log(`Welcome to Round ${index}`)
+        console.log(`The score is You-${humanScore} to Computer-${computerScore}`)
+        playRound(getHumanChoice(), getComputerChoice())
+        if(index === 5){
+            console.log(`GAME OVER ---------- Final Score - You-${humanScore} to Computer-${computerScore}`)
+        }
+    }
+}
+
+
+function getComputerChoice(){
+    let randomNum = Math.random()
+    if(randomNum <= 0.33){
+        return "rock"
+    }else if(randomNum <= 0.66){
+        return "paper"
+    }else{
+        return "scissors"
+    }
+}
+
+
+function errorCheckForHuman(e){
+    if (e === "paper" || e == "scissors" || e === "rock"){
+        return true
+    }else{
+        return false
+    }    
+}
+
+function getHumanChoice(){
+    let humanChoice = prompt("Choose Rock, Paper, or Scissors")
+    if(errorCheckForHuman(humanChoice.toLowerCase())){
+        return humanChoice
+    } else{
+        return getHumanChoice()
+    }
+}
+
+playGame()
