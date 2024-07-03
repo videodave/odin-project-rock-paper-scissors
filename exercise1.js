70de2b6054961379e083d8d02e89a3b4a4c56fdd
videodave <13732631+videodave@users.noreply.github.com>
2024-07-03 14:55:04 -0500

    all logic working

--- exercise1.js
+++ exercise1.js
@@ -2,6 +2,10 @@
 let humanScore = 0;
 let computerScore = 0;
 
+let round = 1
+
+let whoWon
+
 let result = Math.round(Math.random() * 3)
 console.log("this is the result - ", result)
 
@@ -17,36 +21,57 @@ function playRound(humanChoice){
         computerChoice= "scissors"
     }
     if(humanChoice === computerChoice){
-        return console.log("You tied")
+        whoWon = "You tied"
     }else if(humanChoice === "rock"){
         if(computerChoice === "paper"){
             computerScore++
+            whoWon = `You lose - ${computerChoice} beats ${humanChoice}`
             console.log(`You lose - ${computerChoice} beats ${humanChoice}`)
         }else{
             humanScore++
+            whoWon = `You win! - ${humanChoice} beats ${computerChoice}`
             console.log(`You win! - ${humanChoice} beats ${computerChoice}`)
         }
     }else if(humanChoice === "paper"){
         if(computerChoice === "scissors"){
             computerScore++
+            whoWon = `You lose - ${computerChoice} beats ${humanChoice}`
             console.log(`You lose - ${computerChoice} beats ${humanChoice}`)
         }else{
             humanScore++
+            whoWon = `You win! - ${humanChoice} beats ${computerChoice}`
             console.log(`You win! - ${humanChoice} beats ${computerChoice}`)
         }
     }else if(humanChoice === "scissors"){
         if(computerChoice === "rock"){
             computerScore++
+            whoWon = `You lose - ${computerChoice} beats ${humanChoice}`
             console.log(`You lose - ${computerChoice} beats ${humanChoice}`)
         }else{
             humanScore++
+            whoWon = `You win! - ${humanChoice} beats ${computerChoice}`
             console.log(`You win! - ${humanChoice} beats ${computerChoice}`)
         }
     }else{
         return console.log("error")
     }
+
+    if(humanScore === 5){
+        endGame("human")
+        return
+    }else if (computerScore === 5){
+        endGame("computer")
+        return
+    }
+
+
     document.getElementById("selection").style.display = "none"
+    document.getElementById("whoWonText").textContent = whoWon
+    document.getElementById("whoWonContainer").style.display = "block"
+    document.getElementById("humanScore").textContent = humanScore
+    document.getElementById("computerScore").textContent = computerScore
     document.getElementById("score").style.display = "block"
+    document.getElementById("nextRound").style.display = "flex"
 }
 
 
@@ -100,4 +125,41 @@ function buttonTest(){
 function startGame(){
     document.getElementById("selection").style.display = "block"
     document.getElementById("startGameBtn").style.display = "none"
+}
+
+function playAgain(){
+    round = 1
+    document.getElementById("roundNum").textContent = round
+    humanScore = 0;
+    computerScore = 0;
+    document.getElementById("startGameBtn").style.display = "flex"
+    document.getElementById("whoWonContainer").style.display = "none"
+      document.getElementById("score").style.display = "none"
+       document.getElementById("playAgain").style.display = "none"
+
+
+}
+
+function nextRound(){
+    round++
+    console.log("this is the round - ", round)
+    document.getElementById("roundNum").textContent = round
+    document.getElementById("whoWonContainer").style.display = "none"
+    document.getElementById("selection").style.display = "block"
+    document.getElementById("nextRound").style.display = "none"
+}
+
+function endGame(winner){
+    if (winner === "human"){
+        document.getElementById("whoWonText").textContent = "You were the first to 5 and WIN!"
+     
+    }else{
+        document.getElementById("whoWonText").textContent = "You LOST - Computer was the first to 5"
+    }
+        document.getElementById("playAgain").style.display = "flex"
+        document.getElementById("selection").style.display = "none"
+        document.getElementById("whoWonContainer").style.display = "block"
+        document.getElementById("humanScore").textContent = humanScore
+        document.getElementById("computerScore").textContent = computerScore
+        document.getElementById("score").style.display = "block"
 }
